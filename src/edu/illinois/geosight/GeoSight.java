2ab973d111496dffdf468952b3cbad0cd7f87fe6
Steven Kabbes <stevenkabbes@gmail.com>
2011-03-09 09:59:23 -0600

    Added sweet home screen, icons and animation

--- src/edu/illinois/geosight/GeoSight.java
+++ src/edu/illinois/geosight/GeoSight.java
@@ -1,17 +1,17 @@
 package edu.illinois.geosight;
 
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import edu.illinois.geosight.maps.GoogleMapActivity;
-import edu.illinois.geosight.servercom.Sight;
 
 public class GeoSight extends Activity implements OnClickListener {
 	
@@ -19,12 +19,19 @@ public class GeoSight extends Activity implements OnClickListener {
 	private Button mapButton;
 	private Button navButton;
 	private Button sightsButton;
+	private ImageView mLogo;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // allows smooth alpha on this window
+        getWindow().setFormat(PixelFormat.RGBA_8888);
+        
         setContentView(R.layout.main);
+        
+        mLogo = (ImageView) findViewById(R.id.logo);
         
         cameraPreview = (Button) findViewById(R.id.CameraPreview);
         cameraPreview.setOnClickListener(this);
@@ -37,6 +44,30 @@ public class GeoSight extends Activity implements OnClickListener {
         
         sightsButton = (Button) findViewById(R.id.SightsButton);
         sightsButton.setOnClickListener(this);
+        
+        startAnimations();
+
+    }
+    
+    private void startAnimations(){
+    	Animation slideInRight = AnimationUtils.loadAnimation(this, R.anim.slide_in_from_right);
+    	Animation slideInLeft = AnimationUtils.loadAnimation(this, R.anim.slide_in_from_left);
+    	
+        mLogo.startAnimation( slideInRight );
+        
+        // start these after first one finishes
+        slideInLeft.setStartOffset( slideInRight.getDuration() );
+        cameraPreview.startAnimation( slideInLeft );
+        
+    	// delay this one 1/3 the total
+        slideInLeft = AnimationUtils.loadAnimation(this, R.anim.slide_in_from_left);
+        slideInLeft.setStartOffset( slideInRight.getDuration() + slideInLeft.getDuration() / 3 );
+        sightsButton.startAnimation( slideInLeft );
+
+        // need to reload this animation, since it'll have a different offset
+        slideInLeft = AnimationUtils.loadAnimation(this, R.anim.slide_in_from_left);
+        slideInLeft.setStartOffset( slideInRight.getDuration() + 2 * slideInLeft.getDuration() / 3 );
+        mapButton.startAnimation( slideInLeft );
     }
     
 	@Override
