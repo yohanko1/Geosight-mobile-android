41954ee4c29102c998809f7d365f0992afd05369
Steven Kabbes <stevenkabbes@gmail.com>
2011-04-17 21:31:24 -0500

    Commented Geosight

--- src/edu/illinois/geosight/GeoSight.java
+++ src/edu/illinois/geosight/GeoSight.java
@@ -23,25 +23,21 @@ public class GeoSight extends Activity implements LoginCallback{
 	private TextView mStatus;
 	
 	// Buttons for menu
-	private Button loginButton;
-	private Button cameraPreview;
-	private Button sightsButton;
-	private Button mapButton;
-	private Button galleryButton;
-	private Button uploadButton;
+	private Button mLoginButton;
+	private Button mCameraButton;
+	private Button mSightsButton;
+	private Button mMapButton;
+	private Button mGalleryButton;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// allows smooth alpha on this window
-		getWindow().setFormat(PixelFormat.RGBA_8888);
 		
+		// allows smooth alpha blending on this window
+		getWindow().setFormat(PixelFormat.RGBA_8888);
 		setContentView(R.layout.main);
-
 		assignViewMembers();
-
-		// start up the entrance animations
 		startAnimations();
 	}
 
@@ -51,70 +47,88 @@ public class GeoSight extends Activity implements LoginCallback{
 	private void assignViewMembers() {
 		// set up private member variable for the view
 		mLogo = (ImageView) findViewById(R.id.logo);
-		loginButton = (Button) findViewById(R.id.LoginButton);
-		cameraPreview = (Button) findViewById(R.id.CameraPreview);
-		sightsButton = (Button) findViewById(R.id.SightsButton);
-		mapButton = (Button) findViewById(R.id.MapButton);
-		galleryButton = (Button) findViewById(R.id.GalleryButton);
+		mLoginButton = (Button) findViewById(R.id.LoginButton);
+		mCameraButton = (Button) findViewById(R.id.CameraPreview);
+		mSightsButton = (Button) findViewById(R.id.SightsButton);
+		mMapButton = (Button) findViewById(R.id.MapButton);
+		mGalleryButton = (Button) findViewById(R.id.GalleryButton);
 		mStatus = (TextView) findViewById(R.id.homeStatus);
-		uploadButton = (Button) findViewById( R.id.UploadTestButton );
 	}
 
-	// Let's go ahead and start the entrance animation for the buttons and logo
+	/**
+	 * Start the entrance animations for the activity
+	 */
 	private void startAnimations() {
-		Animation slideInRight = AnimationUtils.loadAnimation(this,
-				R.anim.slide_in_from_right);
+		Animation slideInRight = AnimationUtils.loadAnimation(this, R.anim.slide_in_from_right);
 
 		mLogo.startAnimation(slideInRight);
 
 		// set up animations for the buttons
 		for (int i = 0; i < 5; i++) {
-			Animation slideInLeft = AnimationUtils.loadAnimation(this,
-					R.anim.slide_in_from_left);
+			Animation slideInLeft = AnimationUtils.loadAnimation(this, R.anim.slide_in_from_left);
 
 			// each slide in left will be delayed slightly from the previous
 			slideInLeft.setStartOffset(slideInRight.getDuration() + i * slideInLeft.getDuration() / 5);
 
 			if (i == 0) {
-				loginButton.startAnimation(slideInLeft);
+				mLoginButton.startAnimation(slideInLeft);
 			} else if (i == 1) {
-				cameraPreview.startAnimation(slideInLeft);
+				mCameraButton.startAnimation(slideInLeft);
 			} else if (i == 2) {
-				sightsButton.startAnimation(slideInLeft);
+				mSightsButton.startAnimation(slideInLeft);
 			} else if (i == 3) {
-				mapButton.startAnimation(slideInLeft);
+				mMapButton.startAnimation(slideInLeft);
 			} else if (i == 4) {
-				galleryButton.startAnimation(slideInLeft);
+				mGalleryButton.startAnimation(slideInLeft);
 			}
 		}
 	}
 	
+	/**
+	 * Action called when the Camera button is clicked
+	 * @param v
+	 */
 	public void onCameraClicked(View v){
 		Intent intent = new Intent(this, GPSCameraActivity.class);
 		startActivity(intent);
 	}
 	
+	/**
+	 * Action called when the Sights Button is clicked
+	 * @param v
+	 */
 	public void onSightsClicked(View v){
 		Intent intent = new Intent(this, SightListActivity.class);
 		startActivity(intent);
 	}
 	
+	/**
+	 * Action called when the Map button is clicked
+	 * @param v
+	 */
 	public void onMapClicked(View v){
 		Intent intent = new Intent(this, GoogleMapActivity.class);
 		startActivity(intent);
 	}
 	
+	/**
+	 * Action called when the Gallery button is clicked
+	 * @param v
+	 */
 	public void onGalleryClicked(View v){
 		Intent intent = new Intent(this, GalleryActivity.class);
 		startActivity(intent);
 	}
 	
+	/**
+	 * Action called when the Login button is clicked
+	 * @param v
+	 */
 	public void onLoginClicked(View v){
 		LoginDialog.show(this, this);
 	}
 	
 
-	// simply here for conveniance later
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -129,6 +143,9 @@ public class GeoSight extends Activity implements LoginCallback{
 		super.onPause();
 	}
 
+	/**
+	 * How to handle a successful user login
+	 */
 	@Override
 	public void onSuccessfulLogin(User user) {
 		mStatus.setText( "Logged in as " + user.getName() );
